ec291ddef6925ad4be51aca0eefee39327bb6ea6
ガルム <80187947+desuboruto@users.noreply.github.com>
2022-10-03 02:04:59 +0900

    add mantine

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next"
+import SRCIcon from "../components/title/src-Icon"
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-full w-full">
+      <div>Home</div>
+      <SRCIcon />
+    </div>
+  )
+}
+
+export default Home
